fb3f4dda26954f31815003236216fa199172c80c
StevenOng <steven@skyconsultancymns.com>
2025-01-16 15:14:01 +0800

    updated

--- controllers/ewalletController.js
+++ controllers/ewalletController.js
@@ -3,6 +3,33 @@
 //server - route, route - controller,  controller-service
 
 const ewalletService = require('../services/ewalletService');
+const asyncHandler = require('express-async-handler');
+//const Wallet = require('../models/walletModel');
+
+/*
+//get balance
+const getBalance = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const wallet = await Wallet.findOne({ where: { user_id: userId } });
+
+    if(!wallet){
+        res.status(404);
+        throw new Error("Wallet not found");
+    }
+    res.status(200).json({ balance: wallet.balance });
+});
+
+//create wallet if no wallet detected for user
+const createWallet = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const wallet = await Wallet.create({
+        user_id: userId,
+        balance: 0,
+    });
+    res.status(201).json(wallet);
+});
+
+*/
 
 //add funds
 const addFunds = async (req, res) => {
@@ -29,6 +56,8 @@ const subtractFunds = async (req, res) => {
 };
 
 module.exports = {
+    //getBalance,
+    //createWallet,
     addFunds,
     subtractFunds,
 };
